373308e33b36e49815eb7ef19d4446db6b69ca8f
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2020-12-17 12:00:21 +0530

    Add files via upload

---
+++ shifting_array_k_times.cpp
@@ -0,0 +1,27 @@
+/*
+Complexity:- O(n)
+No extra space needed, not even O(1)
+Input:-
+5 2
+1 2 3 4 5
+Output:-
+3 4 5 1 2
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int a[100000];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<k/2;i++)
+        swap(a[i],a[k-i-1]);
+    for(int i=k;i<(n+k)/2;i++)  
+        swap(a[i],a[n+k-i-1]);
+    for(int i=0;i<n/2;i++)
+        swap(a[i],a[n-i-1]);
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    return 0;
+}
